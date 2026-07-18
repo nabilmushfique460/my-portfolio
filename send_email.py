bd01b22703f76d3e42d244566637aff145a6834e
NABIL <nabilmushfique460@gmail.com>
2026-07-18 14:44:18 +0600

    Another Privacy Update

--- send_email.py
+++ send_email.py
@@ -2,15 +2,23 @@ import smtplib
 import ssl
 import os
 
+
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "nabil29089@gmail.com"
+    # Get username and password from Streamlit secrets
+    username = os.getenv("USERNAME")
     password = os.getenv("PASSWORD")
 
-    receiver = "nabil29089@gmail.com"
+    # The receiver can be the same as the username
+    receiver = username
 
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
